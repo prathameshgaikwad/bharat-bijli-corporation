4b49ed6fac3b84f075b9aee9d1ff5e01c9d93f3c
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-09-25 17:47:14 +0530

    feat: Setup TransactionController

---
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/controllers/TransactionController.java
@@ -0,0 +1,26 @@
+package com.prathameshShubham.bharatBijliCorporation.controllers;
+
+import com.prathameshShubham.bharatBijliCorporation.models.Transaction;
+import com.prathameshShubham.bharatBijliCorporation.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @PostMapping
+    public ResponseEntity<Transaction> saveTransaction(@RequestBody Transaction transaction) {
+        return ResponseEntity.ok(transactionService.saveTransaction(transaction));
+    }
+
+    @GetMapping("{transaction-id}")
+    public ResponseEntity<Transaction> getTransaction(@PathVariable("transaction-id") Long transactionId) {
+        return ResponseEntity.ok(transactionService.getTransaction(transactionId));
+    }
+
+}
