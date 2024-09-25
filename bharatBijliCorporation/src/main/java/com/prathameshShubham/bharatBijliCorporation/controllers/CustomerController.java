a27670cc6933cb5b77192377c9cf56e0b3588ceb
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-09-25 23:05:51 +0530

    feat: setup CustomerController

---
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/controllers/CustomerController.java
@@ -0,0 +1,30 @@
+package com.prathameshShubham.bharatBijliCorporation.controllers;
+
+import com.prathameshShubham.bharatBijliCorporation.models.Customer;
+import com.prathameshShubham.bharatBijliCorporation.models.CustomerRequest;
+import com.prathameshShubham.bharatBijliCorporation.models.PersonalDetails;
+import com.prathameshShubham.bharatBijliCorporation.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<Customer> saveCustomer(@RequestBody CustomerRequest customerRequest) {
+        PersonalDetails personalDetails = customerRequest.getPersonalDetails();
+        Customer customer = customerRequest.getCustomer();
+
+        return ResponseEntity.ok(customerService.saveCustomer(customer, personalDetails));
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable Long customerId){
+        return ResponseEntity.ok(customerService.getCustomer(customerId));
+    }
+}
---
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/models/CustomerRequest.java
@@ -0,0 +1,11 @@
+package com.prathameshShubham.bharatBijliCorporation.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CustomerRequest {
+    private Customer customer;
+    private PersonalDetails personalDetails;
+}
