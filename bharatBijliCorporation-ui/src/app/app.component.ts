9e9da67b183bfac58cde08565460bae64d56ba0f
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-09-28 00:40:06 +0530

    feat: customer registration form

--- bharatBijliCorporation-ui/src/app/app.component.ts
+++ bharatBijliCorporation-ui/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ bharatBijliCorporation-ui/src/app/auth/register/register.component.ts
@@ -0,0 +1,53 @@
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputTextModule } from 'primeng/inputtext';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FloatLabelModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  protected registrationForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    emailId: new FormControl('', [Validators.required, Validators.email]),
+    phoneNumber: new FormControl('', [
+      Validators.required,
+      Validators.minLength(10),
+    ]),
+    address: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    pincode: new FormControl('', [
+      Validators.minLength(6),
+      Validators.maxLength(6),
+    ]),
+    dateOfBirth: new FormControl(null, [Validators.required]),
+  });
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+  }
+}
