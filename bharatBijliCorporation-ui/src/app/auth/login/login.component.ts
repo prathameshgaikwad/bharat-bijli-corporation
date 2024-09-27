90599455ae23715e7878189e8426b09f0521717a
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-09-27 17:57:39 +0530

    feat: define & connect auth service

--- bharatBijliCorporation-ui/src/app/auth/login/login.component.ts
+++ bharatBijliCorporation-ui/src/app/auth/login/login.component.ts
@@ -1,13 +1,52 @@
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { AuthService } from '../../shared/services/auth.service';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 import { CheckboxModule } from 'primeng/checkbox';
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CheckboxModule, InputTextModule, ButtonModule],
+  imports: [
+    CheckboxModule,
+    InputTextModule,
+    ButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CardModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected loginForm = new FormGroup({
+    customerId: new FormControl('', [Validators.required]),
+  });
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    const customerId = this.loginForm.get('customerId')?.value || '';
+
+    this.authService.login(customerId).subscribe({
+      next: (response) => {
+        console.log('Login successful', response);
+        // Optionally store user info or token and navigate to dashboard
+        this.router.navigate(['/dashboard']);
+      },
+      error: (error) => {
+        console.error('Login failed', error);
+      },
+    });
+  }
+}
