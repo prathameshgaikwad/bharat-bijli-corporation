bc3b9c866a9db7150735354ae50f4f66332edbf4
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-09-28 23:20:39 +0530

    feat: solves #27 Customer Actions Menu

--- bharatBijliCorporation-ui/src/app/customer/customer-navbar/customer-actions-menu/customer-actions-menu.component.ts
+++ bharatBijliCorporation-ui/src/app/customer/customer-navbar/customer-actions-menu/customer-actions-menu.component.ts
@@ -1,16 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+
 import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
-import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-customer-actions-menu',
   standalone: true,
-  imports: [AvatarModule, ButtonModule, RouterModule],
+  imports: [AvatarModule, ButtonModule, RouterModule, MenuModule],
   templateUrl: './customer-actions-menu.component.html',
   styleUrl: './customer-actions-menu.component.css',
 })
-export class CustomerActionsMenuComponent {
+export class CustomerActionsMenuComponent implements OnInit {
   balance: number = 1203.9;
   username: string = 'Prathamesh';
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Profile',
+        icon: 'pi pi-user',
+      },
+      {
+        label: 'Payment Methods',
+        icon: 'pi pi-wallet',
+      },
+      {
+        label: 'Settings',
+        icon: 'pi pi-cog',
+      },
+      {
+        separator: true,
+      },
+      {
+        label: 'Logout',
+        icon: 'pi pi-sign-out',
+      },
+    ];
+  }
 }
