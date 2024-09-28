881063037719b2325a361f92667ac098c5d453b2
Shubham <shubhamkaspate213@gmail.com>
2024-09-28 18:08:07 +0530

    Ignore_Emp_package

--- bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/models/Invoice.java
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/models/Invoice.java
@@ -1,6 +1,7 @@
 package com.prathameshShubham.bharatBijliCorporation.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.prathameshShubham.bharatBijliCorporation.enums.InvoiceStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
