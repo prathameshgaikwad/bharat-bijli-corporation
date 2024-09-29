fba6d2d56a15adfff6564e4368800ec32680214b
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-09-29 18:14:37 +0530

    feat: Customer Registration endpoint (#51)

--- bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/services/CustomerService.java
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/services/CustomerService.java
@@ -8,11 +8,10 @@ import com.prathameshShubham.bharatBijliCorporation.models.PersonalDetails;
 import com.prathameshShubham.bharatBijliCorporation.repositories.CustomerRepo;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -30,15 +29,19 @@ public class CustomerService {
     private PersonalDetailsService personalDetailsService;
 
     public Customer saveCustomer(PersonalDetails personalDetails) {
-        PersonalDetails savedPersonalDetails = personalDetailsService.savePersonalDetails(personalDetails);
-        String customerId = generateCustomerId();
+        try {
+            PersonalDetails savedPersonalDetails = personalDetailsService.savePersonalDetails(personalDetails);
+            String customerId = generateCustomerId();
 
-        Customer newCustomer  = new Customer();
-        newCustomer.setId(customerId);
-        newCustomer.setPersonalDetails(savedPersonalDetails);
-        newCustomer.setServiceConnectionStatus(ServiceConnectionStatus.ACTIVE);
+            Customer newCustomer  = new Customer();
+            newCustomer.setId(customerId);
+            newCustomer.setPersonalDetails(savedPersonalDetails);
+            newCustomer.setServiceConnectionStatus(ServiceConnectionStatus.ACTIVE);
 
-        return customerRepo.save(newCustomer);
+            return customerRepo.save(newCustomer);
+        } catch (DataIntegrityViolationException exception) {
+            throw new DataIntegrityViolationException(exception.getMessage());
+        }
     }
 
     // Service to save a batch of customers
