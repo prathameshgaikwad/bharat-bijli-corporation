1aea0f7a1a16bc29ef200ab66327f0ff17dd05c1
Shubham <shubhamkaspate213@gmail.com>
2024-09-30 19:34:15 +0530

    Adding-Required-Exceptions

---
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/exceptions/InvalidIdFormatException.java
@@ -0,0 +1,7 @@
+package com.prathameshShubham.bharatBijliCorporation.exceptions;
+
+public class InvalidIdFormatException extends RuntimeException {
+    public InvalidIdFormatException(String message) {
+        super(message);
+    }
+}
