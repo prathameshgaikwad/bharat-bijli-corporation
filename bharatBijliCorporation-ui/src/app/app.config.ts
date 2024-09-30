4429c7e7f6764fa1d73a26da24c8dba43afe3541
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-09-30 19:18:45 +0530

    fix app.config.ts for interceptors to work

--- bharatBijliCorporation-ui/src/app/app.config.ts
+++ bharatBijliCorporation-ui/src/app/app.config.ts
@@ -1,7 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  HTTP_INTERCEPTORS,
+  provideHttpClient,
+  withInterceptors,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
+import { AuthInterceptor } from './core/interceptors/auth.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
@@ -9,7 +15,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(),
     provideAnimations(),
+    provideHttpClient(withInterceptorsFromDi()),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
 };
