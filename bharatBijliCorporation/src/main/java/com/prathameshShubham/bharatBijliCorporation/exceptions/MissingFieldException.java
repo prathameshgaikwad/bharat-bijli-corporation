5c0a3219afcb9b5bd462a7e70ee8450c6b02bef5
Shubham <shubhamkaspate213@gmail.com>
2024-09-30 17:46:49 +0530

    Adding-Required-Exceptions

---
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/exceptions/MissingFieldException.java
@@ -0,0 +1,7 @@
+package com.prathameshShubham.bharatBijliCorporation.exceptions;
+
+public class MissingFieldException  extends Exception {
+    public MissingFieldException(String message) {
+        super(message);
+    }
+}
