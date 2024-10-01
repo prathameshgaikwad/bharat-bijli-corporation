77637a1b2afc76de4604c62423893e1eb8b164c9
Shubham <shubhamkaspate213@gmail.com>
2024-10-02 03:33:32 +0530

    Emp-service

--- bharatBijliCorporation-ui/src/app/employee/services/employee.service.ts
+++ bharatBijliCorporation-ui/src/app/employee/services/employee.service.ts
@@ -3,12 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UsernameResponse } from '../../shared/types/auth.types';
+import { Page, Transaction } from '../../shared/types/consumables.types';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmployeeService {
   private baseUrl = 'http://localhost:8080/employees';
+  private baseTransactionsUrl = 'http://localhost:8080/transactions';
 
   constructor(private httpClient: HttpClient) {}
 
@@ -20,5 +22,9 @@ export class EmployeeService {
   getEmployeeUsername(employeeId: string): Observable<UsernameResponse> {
     const url = `${this.baseUrl}/${employeeId}/username`;
     return this.httpClient.get<UsernameResponse>(url);
+  }
+
+  getRecentTransactions() : Observable<Page<Transaction>>{
+    return this.httpClient.get<Page<Transaction>> (`${this.baseTransactionsUrl}/recents`);
   }
 }
