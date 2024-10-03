ea82055ca25cc8d4a1ecb9358715f015b94660b9
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-10-03 23:57:53 +0530

    feat: Improved Cash Payment Dialog

--- bharatBijliCorporation-ui/src/app/customer/invoices/invoice-payment-page/cash-payment/cash-payment.component.ts
+++ bharatBijliCorporation-ui/src/app/customer/invoices/invoice-payment-page/cash-payment/cash-payment.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 
 import { DialogModule } from 'primeng/dialog';
 import { DividerModule } from 'primeng/divider';
+import { FieldsetModule } from 'primeng/fieldset';
+import { PanelModule } from 'primeng/panel';
 
 @Component({
   selector: 'app-cash-payment',
   standalone: true,
-  imports: [DialogModule, DividerModule],
+  imports: [DialogModule, DividerModule, PanelModule, FieldsetModule],
   templateUrl: './cash-payment.component.html',
   styleUrl: './cash-payment.component.css',
 })
