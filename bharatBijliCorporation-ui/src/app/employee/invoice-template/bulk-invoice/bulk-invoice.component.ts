d9b8ff2535f5a0f4e004c24ad7157e48cee4e0c8
Shubham <shubhamkaspate213@gmail.com>
2024-10-04 16:19:38 +0530

    Invoices-Updates

---
+++ bharatBijliCorporation-ui/src/app/employee/invoice-template/bulk-invoice/bulk-invoice.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bulk-invoice',
+  standalone: true,
+  imports: [],
+  templateUrl: './bulk-invoice.component.html',
+  styleUrl: './bulk-invoice.component.css'
+})
+export class BulkInvoiceComponent {
+
+}
