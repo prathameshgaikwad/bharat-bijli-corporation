2e6af38fdfda34f112dbe2a8e155add2d81b68b3
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-10-04 15:46:55 +0530

    feat: View transaction summary (#104)

---
+++ bharatBijliCorporation-ui/src/app/customer/payments/payment-summary/payment-summary.component.ts
@@ -0,0 +1,58 @@
+import { CommonModule, DatePipe } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+import { DEFAULT_TRANSACTION } from '../../../core/helpers/constants';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { TagModule } from 'primeng/tag';
+import { Transaction } from '../../../shared/types/consumables.types';
+
+@Component({
+  selector: 'app-payment-summary',
+  standalone: true,
+  imports: [DialogModule, TagModule, CommonModule, DatePipe, DividerModule],
+  templateUrl: './payment-summary.component.html',
+  styleUrl: './payment-summary.component.css',
+})
+export class PaymentSummaryComponent {
+  @Input({}) isVisible: boolean = false;
+  @Input({}) transactionDetails: Transaction = DEFAULT_TRANSACTION;
+
+  getSeverity(status: string) {
+    switch (status) {
+      case 'SUCCESS':
+        return 'success';
+      case 'PENDING':
+        return 'warning';
+      case 'CANCELLED':
+        return 'secondary';
+      case 'FAILED':
+        return 'danger';
+      case 'DECLINED':
+        return 'danger';
+      case 'EXPIRED':
+        return 'secondary';
+      default:
+        return 'info';
+    }
+  }
+
+  getIcon(status: string) {
+    switch (status) {
+      case 'SUCCESS':
+        return 'pi pi-check-circle';
+      case 'PENDING':
+        return 'pi pi-clock';
+      case 'CANCELLED':
+        return 'pi pi-times-cross';
+      case 'FAILED':
+        return 'pi pi-times';
+      case 'DECLINED':
+        return 'pi pi-ban';
+      case 'EXPIRED':
+        return 'pi pi-hourglass';
+      default:
+        return 'pi pi-clock';
+    }
+  }
+}
