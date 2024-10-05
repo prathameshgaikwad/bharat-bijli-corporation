ba32dc73c904deb773ef18e31da50acdb8158f63
Shubham <shubhamkaspate213@gmail.com>
2024-10-05 21:08:09 +0530

    Paginated-Customer-Update-it

--- bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/repositories/PersonalDetailsRepo.java
+++ bharatBijliCorporation/src/main/java/com/prathameshShubham/bharatBijliCorporation/repositories/PersonalDetailsRepo.java
@@ -4,6 +4,9 @@ import com.prathameshShubham.bharatBijliCorporation.models.PersonalDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PersonalDetailsRepo extends JpaRepository<PersonalDetails, Long> {
+    Optional<PersonalDetails> findByEmailId(String emailId);
 }
